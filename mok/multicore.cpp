123507e8a5f099218a414e63a55b6bb087ed75c6
bartbal <bartvannetburg@hotmail.com>
2023-03-31 12:29:29 +0200

    added proper comments

--- mok/multicore.cpp
+++ mok/multicore.cpp
@@ -4,7 +4,7 @@
  * File Created: Thursday, 30th March 2023 10:21:34 am
  * Author: Bart van Netburg (bartvannetburg@hotmail.com)
  * -----
- * Last Modified: Friday, 31st March 2023 11:49:02 am
+ * Last Modified: Friday, 31st March 2023 12:27:38 pm
  * Modified By: Bart van Netburg (bartvannetburg@hotmail.com>)
  * -----
  * Copyright 2023 - 2023 B.J.G. van Netburg
@@ -26,18 +26,24 @@ extern "C"{
 static std::atomic<bool> quit(false);
 static std::atomic<bool> wait(true);
 
+/**
+ * @brief 
+ * Function to call when ctrl+c is presed.
+ * Assigned in the multicore_launch_core1 function
+ */
 static void got_signal(int){
     quit.store(true);
 }
 
 void multicore_launch_core1(void (*entry)(void)){
-
+    // catch when ctrl+c is pressed and call got_signal method
     struct sigaction sa;
     memset( &sa, 0, sizeof(sa) );
     sa.sa_handler = got_signal;
     sigfillset(&sa.sa_mask);
     sigaction(SIGINT,&sa,NULL);
 
+    // launch thread and detach
     std::thread thread_obj(entry);
     thread_obj.detach();
 }
--- mok/pico/multicore.h
+++ mok/pico/multicore.h
@@ -4,7 +4,7 @@
  * File Created: Thursday, 30th March 2023 10:19:22 am
  * Author: Bart van Netburg (bartvannetburg@hotmail.com)
  * -----
- * Last Modified: Friday, 31st March 2023 11:48:37 am
+ * Last Modified: Friday, 31st March 2023 12:29:00 pm
  * Modified By: Bart van Netburg (bartvannetburg@hotmail.com>)
  * -----
  * Copyright 2023 - 2023 B.J.G. van Netburg
@@ -14,15 +14,49 @@
 #ifndef MULTICORE_H
 #define MULTICORE_H
 
+/**
+ * @brief 
+ * Use this in the loop of the threaded task to check when to stop the thread.
+ * @return bool 
+ * True if thread has to stop
+ */
 bool multicore_thread_exit();
 
+/**
+ * @brief 
+ * Use this in the main loop to check when to exit.
+ * @return bool
+ * True if main loop has to stop
+ */
 bool multicore_main_exit();
 
+/**
+ * @brief 
+ * Use this method in sub loops of any thread loops or the main 
+ * loop to check if the loop has to stop.
+ * @return bool
+ * True if the loop has to stop
+ */
 bool multicore_subs_exit();
 
+/**
+ * @brief 
+ * sdk mok method for launching core 1.
+ * @details
+ * This method starts a new thread instead of a core. 
+ * Will also catch ctrl+c input to stop threads. For that to work 
+ * properly make sure to use the multicore exit methods. 
+ * @param entry 
+ * Function pointer to a funtion to run in a seperate "core" 
+ * which in this case is actualy a thread.
+ */
 void multicore_launch_core1(void (*entry)(void));
 
-// FIXME: not a method of multicore but didn't know where else to put it for now
+/**
+ * @brief 
+ * This method gets called in the main loop.
+ * This mok version just returns.
+ */
 void tight_loop_contents();
 
 #endif // MULTICORE_H

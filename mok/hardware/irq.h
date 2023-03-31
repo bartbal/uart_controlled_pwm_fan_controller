20fda7d8209304470d93a73b776545921fa03c90
bartbal <bartvannetburg@hotmail.com>
2023-03-31 13:23:15 +0200

    irq now simulates the original and has proper comments

--- mok/hardware/irq.h
+++ mok/hardware/irq.h
@@ -4,7 +4,7 @@
  * File Created: Thursday, 30th March 2023 10:20:42 am
  * Author: Bart van Netburg (bartvannetburg@hotmail.com)
  * -----
- * Last Modified: Thursday, 30th March 2023 6:08:53 pm
+ * Last Modified: Friday, 31st March 2023 1:19:41 pm
  * Modified By: Bart van Netburg (bartvannetburg@hotmail.com>)
  * -----
  * Copyright 2023 - 2023 B.J.G. van Netburg
@@ -16,10 +16,26 @@
 #include "types.h"
 #include <stdlib.h>
 
+// declare irq handler type
 typedef void (*irq_handler_t)(void);
 
+/**
+ * @brief 
+ * Mok setting interrupt. This creates a thread that calls the handler every half a second.
+ * it also checks if the correct num was passed.
+ * @param num 
+ * @param handler 
+ */
 void irq_set_exclusive_handler(uint num, irq_handler_t handler);
 
+/**
+ * @brief 
+ * Mok the set enabled function. checks paramters.
+ * if irq is not enabled the irq handler set with 
+ * irq_set_exclusive_handler will not be called.
+ * @param num 
+ * @param enabled 
+ */
 void irq_set_enabled(uint num, bool enabled);
 
 #endif // IRQ_H
--- mok/irq.cpp
+++ mok/irq.cpp
@@ -4,7 +4,7 @@
  * File Created: Thursday, 30th March 2023 10:21:25 am
  * Author: Bart van Netburg (bartvannetburg@hotmail.com)
  * -----
- * Last Modified: Thursday, 30th March 2023 9:41:03 pm
+ * Last Modified: Friday, 31st March 2023 1:19:21 pm
  * Modified By: Bart van Netburg (bartvannetburg@hotmail.com>)
  * -----
  * Copyright 2023 - 2023 B.J.G. van Netburg
@@ -15,14 +15,47 @@
 extern "C"{
     #include "hardware/irq.h"
     #include <stdlib.h>
+    #include "pico/multicore.h"
+    #include "pico/time.h"
 }
+
+#include <thread>
 #include <cstdio>
+
+// used to store the handle given by irq_set_exclusive_handler.
+// Cannot pass this as argument so had to wrap it in irq_loop.
+static irq_handler_t _handler;
+
+// used to test if irq has been set to enabled
+static bool _enabled = false;
+
+/**
+ * @brief 
+ * Moks the on wrap interrupt. This calls the irq_handler 
+ * about as many times the original interupt does
+ */
+static void irq_loop(){
+    while(true){
+        if(multicore_subs_exit()){
+            break;
+        }
+        if(_enabled){
+            _handler();
+        } else {
+            printf("irq not enabled, call irq_set_enabled to enable\n");
+        }
+        sleep_us(500000);
+    }
+}
 
 void irq_set_exclusive_handler(uint num, irq_handler_t handler){
     if(num != 4){
         printf("First param should be 4!\n");
     }
-    // TODO: handler should be called periodicly
+    _handler = handler;
+    // launch thread and detach
+    std::thread thread_obj(irq_loop);
+    thread_obj.detach();
 }
 
 void irq_set_enabled(uint num, bool enabled){
@@ -32,6 +65,7 @@ void irq_set_enabled(uint num, bool enabled){
     if(!enabled){
         printf("irq set enabled should be enabled!");
     }
+    _enabled = enabled;
 }
 
 
